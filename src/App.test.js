c815dac2bf2a9099a3f1c8d76310ca3115d72c26
truejojo <johannesveh@yahoo.de>
2022-11-04 09:14:21 +0100

    add the functionaity to the app

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { logRoles } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial color", () => {
-  const {container} = render(<App />);
+test("button has correct initial color, and updates when click", () => {
+  const { container } = render(<App />);
   logRoles(container);
 
   // find an element with a role of button and text of "Change to blue"
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
   // expect the background color to be red
-  expect(colorButton).toHaveStyle({
-    backgroundColor: "red",
-  });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton).toHaveTextContent("Change to red");
 });
-test("button turns blue when clicked", () => {});
